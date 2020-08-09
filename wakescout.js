612f19f903b5271071222813099c0634ec085f0e
Asbjørn Christian Bach <43601099+acbach@users.noreply.github.com>
2020-08-09 19:02:04 +0200

    Update to new webpage

--- wakescout.js
+++ wakescout.js
@@ -6,17 +6,17 @@ const fs = require("fs");
   try {
     // open the headless browser
     browser = await puppeteer.launch(
-      { headless: true },
+      { headless: false },
       { waitUntil: "networkidle2" }
     );
 
-    var startId = 2830;
+    var startId = 3133;
     var batchSize = 10;
 
     while (startId < 4000) {
       const promises = [];
       for (let i = 0; i < batchSize; i++) {
-        const promise = new Promise(async function(resolve, reject) {
+        const promise = new Promise(async function (resolve, reject) {
           try {
             const page = await browser.newPage();
 
@@ -26,58 +26,68 @@ const fs = require("fs");
             //http://www.wakescout.com/listing/1
             var link = `http://www.wakescout.com/listing/${id}`;
             await page.goto(link, {
-              timeout: 0
+              timeout: 0,
             });
             // await page.waitForSelector(".location-header--types");
 
             const wakepark = await page.evaluate(() => {
-              if (document.querySelector(`.location-header--types`) == null)
-                return;
+              if (document.querySelector(`#location_about`) == null) return;
               var types = document
-                .querySelector(`.location-header--types`)
+                .querySelector(
+                  `#wrapper > section.mb-4.mb-md-6.mb-lg-10.px-3.px-md-4.px-lg-5.pt-3.pt-md-0 > div > div > article > ul`
+                )
                 .innerText.trim();
-              if (!types.includes("Cable Wake Park")) return;
+              if (!types.includes("Cable Wake Parks")) return;
 
               var name = document
-                .querySelector(`.col-md-12.col-lg-8 h1`)
-                .innerText.trim();
-
-              var contactInfoList = document.querySelectorAll(
-                `.col-sm-12.col-md-6 ul li`
-              );
-
-              var address = document
-                .querySelectorAll(`.col-sm-12.col-md-6 table`)[0]
-                .innerText.trim()
-                .replace(/\n/gi, " ");
-
-              var phone = document
-                .querySelectorAll(`.col-sm-12.col-md-6 p a`)[0]
+                .querySelector(
+                  `#wrapper > section.mb-4.mb-md-6.mb-lg-10.px-3.px-md-4.px-lg-5.pt-3.pt-md-0 > div > div > article > h1`
+                )
                 .innerText.trim();
 
               var obj = {};
               obj.name = name;
               obj.types = types;
-              obj.phone = phone;
-              obj.address = address;
-              var addressSplit = address.split(" ");
-              obj.country = addressSplit[addressSplit.length - 1];
-              obj.website = contactInfoList[0].innerText.trim().split(" ")[1];
-              obj.email = contactInfoList[1].innerText.trim().split(" ")[1];
-              if (contactInfoList[2].innerText.includes("Facebook")) {
-                obj.facebook = contactInfoList[2].innerText
-                  .trim()
-                  .split(" ")[1];
-              } else {
-                var alternateemail = contactInfoList[2].innerText
-                  .trim()
-                  .split(" ");
-                if (alternateemail.length < 3)
-                  obj.alternateemail = alternateemail[2];
-                obj.facebook = contactInfoList[3].innerText
-                  .trim()
-                  .split(" ")[1];
+
+              var phone = document.querySelector(
+                `#location_address > dl > dd:nth-child(4) > a`
+              );
+
+              if (phone != undefined) obj.phone = phone.innerText.trim();
+
+              var address = document.querySelector(
+                `#location_address > p > span:nth-child(2)`
+              );
+
+              if (address != undefined) {
+                obj.address = address.innerText.trim().replace(/\n/gi, " ");
+                obj.country = address.split(" ")[addressSplit.length - 1];
               }
+
+              var website = document.querySelector(
+                `body > section > div > section > div > div.col-xs-12.col-sm-8 > div > div.row.text-page > div:nth-child(1) > ul > li:nth-child(1) > a`
+              );
+              if (website != undefined) obj.website = website.innerText.trim();
+
+              var email = document.querySelector(
+                `#location_address > dl > dd:nth-child(2) > a`
+              );
+              if (email != undefined) obj.email = email.innerText.trim();
+
+              // if (contactInfoList[2].innerText.includes("Facebook")) {
+              //   obj.facebook = contactInfoList[2].innerText
+              //     .trim()
+              //     .split(" ")[1];
+              // } else {
+              //   var alternateemail = contactInfoList[2].innerText
+              //     .trim()
+              //     .split(" ");
+              //   if (alternateemail.length < 3)
+              //     obj.alternateemail = alternateemail[2];
+              //   obj.facebook = contactInfoList[3].innerText
+              //     .trim()
+              //     .split(" ")[1];
+              // }
               return obj;
             });
 
@@ -88,7 +98,7 @@ const fs = require("fs");
               fs.writeFile(
                 `data/wakescout/28012020/${id}.json`,
                 JSON.stringify(wakepark),
-                function(err) {
+                function (err) {
                   if (err) throw err;
                   console.log("Saved!");
                 }
